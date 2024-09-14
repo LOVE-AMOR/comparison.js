ce31ac4fae902051cdb5390b1337fe98f0fed6ff
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-09-13 20:37:00 -0800

    Iniial commit

---
+++ comparison.js
@@ -0,0 +1,14 @@
+let firstFavNum = 16;
+let secondFavNum = 10;
+
+console.log("Greater than:", firstFavNum > secondFavNum);
+console.log("Less than:", firstFavNum < secondFavNum);
+console.log("Greater than or equal to:", firstFavNum >= secondFavNum);
+console.log("Less than  or equal to:", firstFavNum <= secondFavNum);
+console.log("Strict equality (===):", firstFavNum === secondFavNum);
+console.log("Loose equality (==):", firstFavNum == secondFavNum);
+console.log("Strict inequality (!==):", firstFavNum !== secondFavNum);
+console.log("Loose inequality (!=):", firstFavNum != secondFavNum);
+console.log("AND:", (firstFavNum > 5) && (secondFavNum < 5));
+console.log("OR:", (firstFavNum > 5) || (secondFavNum > 5));
+console.log("NOT:", !(firstFavNum === secondFavNum));
